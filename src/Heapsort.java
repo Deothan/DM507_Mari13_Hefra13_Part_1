e3e2820f9557d024705daa5180d619904f5b3406
Deothan <mads@deothan.dk>
2015-04-05 16:14:29 +0200

    Corrected stuff

---
+++ src/Heapsort.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Heapsort {
+    public static void main(String[] args) 
+    {
+        Scanner scan = new Scanner( System.in );
+
+        String[] input = scan.nextLine().split(" ");
+        PQHeap heap = new PQHeap(input.length);
+        
+        for(String s : input){
+            heap.insert(new Element(Integer.parseInt(s), new Object()));
+        }
+
+        System.out.println(heap.ExtractMin().key);
+    } 
+}
